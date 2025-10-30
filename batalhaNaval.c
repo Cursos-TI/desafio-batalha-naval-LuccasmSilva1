37ed9992705b3ee6827e6b143c0f332305523b2b
LuccasmSilva1 <wwoon7314@gmail.com>
2025-10-30 15:28:35 +0000

    novato fazendo o limite

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int i, j;
+    int agua = 0;
+    int navio = 3;
+    int mapa[10][10];
+    int tam_navio = 3;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // tabuleiro 
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            mapa[i][j] = agua;
+        }
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //  Coordenadas
+    int linha_h = 2, coluna_h = 4; // horizontal
+    int linha_v = 5, coluna_v = 7; // vertical
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // limites 
+    if (coluna_h + tam_navio > 10) {
+        printf("Fora dos limites!\n");
+        return 1;
+    }
+
+    if (linha_v + tam_navio > 10) {
+        printf("Fora dos limites!\n");
+        return 1;
+    }
+
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
 
     return 0;
 }
